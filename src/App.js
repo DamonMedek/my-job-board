15e34f64d8eea9a04be80cb48962a5eccd8a8536
Damon medek <68669987+DamonMedek@users.noreply.github.com>
2022-06-20 14:15:13 -0700

    Rough Draft Loaded

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Month from "./components/Month";
+import Section from "./components/Section";
+import Services from "./components/Services";
+import Footer from "./components/Footer";
+import Form from "./components/Form";
 
 export default function App() {
   return (
     <>
       <Navbar />
+      <Hero />
+      <Form />
+      <Month />
+      <Section />
+      <Services />
+      <Footer />
     </>
   );
 }
