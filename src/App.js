7b45372d09cce6ff1a51f5abe5b2986290822a2c
Damon medek <68669987+DamonMedek@users.noreply.github.com>
2022-06-20 12:43:14 -0700

    general navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-
-import tw from "tailwind-styled-components"
-
-const Greeting = tw.h1`
-text-xs
-font-medium
-shadow-sm
-text-blue-900
-hover:bg-purple-700
-focus:outline-none`
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
-    <Greeting>
-      Hello world!
-    </Greeting>
-  )
+    <>
+      <Navbar />
+    </>
+  );
 }
